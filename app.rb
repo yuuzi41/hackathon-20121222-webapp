9b01bc2f51f5fe50d03a4727b2a68e680062d35c
yuuzi <yuuzi41@hotmail.com>
2012-12-22 17:03:11 +0900

    change schema

--- app.rb
+++ app.rb
@@ -1,35 +1,45 @@
 require 'rubygems'
 require 'sinatra'
+require 'erb'
 
 require 'pg'
 
 get '/' do
 	conn = PGconn.connect('ec2-54-243-248-219.compute-1.amazonaws.com',	5432, '','','d8stnhug3r00sl','sqykholuteropu','Z8pzL6QAtHO4n37cFtKoMZc9CS')
-	conn.exec('create table if not exists mark_log (id serial, title text, url text, create_at timestamp)')
+	conn.exec('create table if not exists mark_log (id serial, title text, url text, count integer, create_at timestamp, modified_at timestamp)')
 
+	sqlret = conn.exec('select title,url,count from mark_log order by count desc')
+	@result = sqlret
 	##
-	'Hello, world.'
+	erb :index
 end
 
 get '/api/add' do
 	conn = PGconn.connect('ec2-54-243-248-219.compute-1.amazonaws.com',	5432, '','','d8stnhug3r00sl','sqykholuteropu','Z8pzL6QAtHO4n37cFtKoMZc9CS')
-	conn.exec('create table if not exists mark_log (id serial, title text, url text, create_at timestamp)')
+	conn.exec('create table if not exists mark_log (id serial, title text, url text, count integer, create_at timestamp, modified_at timestamp)')
 
 	##
 	url = params['url']
 	title = params['title']
 
-	conn.exec("insert into mark_log (title,url,create_at) values ('#{url}', '#{title}', CURRENT_TIMESTAMP)")
+	sqlret = conn.exec("select count(*) from mark_log where url = '#{url}'")
+
+	if sqlret[0]['count'].to_i > 0
+		conn.exec("update mark_log set title = '#{title}',count = count+1, modified_at = CURRENT_TIMESTAMP where url = '#{url}'")
+	else
+		conn.exec("insert into mark_log (title,url,count,create_at,modified_at) values ('#{title}', '#{url}', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)")
+	end
+
 	"{'status': 'ok'}"
 end
 
 get '/api/cnt' do
 	conn = PGconn.connect('ec2-54-243-248-219.compute-1.amazonaws.com',	5432, '','','d8stnhug3r00sl','sqykholuteropu','Z8pzL6QAtHO4n37cFtKoMZc9CS')
-	conn.exec('create table if not exists mark_log (id serial, title text, url text, create_at timestamp)')
+	conn.exec('create table if not exists mark_log (id serial, title text, url text, count integer, create_at timestamp, modified_at timestamp)')
 
 	##
 	url = params['url']
-	sqlret = conn.exec("select count(*) from mark_log where url = '#{url}'")
+	sqlret = conn.exec("select count from mark_log where url = '#{url}'")
 
 	"{'count': #{sqlret[0]['count']}}"
 end
